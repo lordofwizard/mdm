c2f94dd6c51a2b329de76857309c06baf8e8e387
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-06-19 11:46:08 +0530

    created working modifiable branch name changing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-use std::io;
-use std::fs::*;
 use colored::*;
+use git2::BranchType;
 use git2::Repository;
-use git2::Branch;
-use git2::BranchType::*;
 
-fn branch_dena(&mut repo: Repository) -> git2::Branch{
-    let br = match &mut repo.find_branch("master",BranchType::Local) {
+
+
+/*fn branch_dena(&mut repo : &mut git2::Repository) -> &mut Branch<'_>{
+    let mut br = match &mut repo.find_branch("master",BranchType::Local) {
         Ok(br ) => br,
         Err(e) => panic!("Some error {}",e)
     };
-}
+    &mut br
+}*/
 
 fn main() {
     let repo = match Repository::open("./data/") {
         Ok(repo) => repo,
         Err(e) => panic!("{} {}","Something went wrong reading the repository \n".red(),e)
     };
-    let branch = match Branch::rename(branch_dena(&mut),"main",true){
-
-    }
-
+    let br= &mut repo.find_branch("master",BranchType::Local).expect("Something went wrong");
+    br.rename("main",true);
     println!("Hello, Gaurav!");
 }
+
