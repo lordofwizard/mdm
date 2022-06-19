0bd22992692caa98f9b5e4d0fa26650b81498824
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-06-19 18:43:13 +0530

    Working upto making today's file functionality

--- src/func.rs
+++ src/func.rs
@@ -1,10 +1,39 @@
+
 // File to store all the basic functionality 
-fn directory_maker(){
+pub mod mdm{
+    use colored::*;
+    use git2::BranchType;
+    use git2::Repository;
 
-}
-fn database_maker(){
+    pub fn run(){
+        let repo = match Repository::open("./data/") {
+            Ok(repo) => repo,
+            Err(e) => panic!("{} {}","Something went wrong reading the repository \n".red(),e)
+        };
+        let br= &mut repo.find_branch("master",BranchType::Local).expect("Something went wrong");
+        br.rename("main",true).expect("Something went wrong while remaning the branch");
+    }
 
-}
-fn data_pusher(){
     
+    fn directory_maker(){
+        
+    }
+    fn database_maker(){
+
+    }   
+    fn data_pusher(){
+
+    }
+    use chrono::*;
+    pub fn date_printer() -> String{
+        let local= Local::today().to_string();
+        local
+    }
+    pub fn making_file(){
+        use std::fs::File;
+        let today = date_printer();
+        let today_str = today.as_str();
+        let path = "./data/".to_string() + &today + ".txt" ;
+        let mut file = File::create(path.as_str());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use colored::*;
-use git2::BranchType;
-use git2::Repository;
-
-
-
-/*fn branch_dena(&mut repo : &mut git2::Repository) -> &mut Branch<'_>{
-    let mut br = match &mut repo.find_branch("master",BranchType::Local) {
-        Ok(br ) => br,
-        Err(e) => panic!("Some error {}",e)
-    };
-    &mut br
-}*/
-
+pub mod func;
+use func::mdm;
 fn main() {
-    let repo = match Repository::open("./data/") {
-        Ok(repo) => repo,
-        Err(e) => panic!("{} {}","Something went wrong reading the repository \n".red(),e)
-    };
-    let br= &mut repo.find_branch("master",BranchType::Local).expect("Something went wrong");
-    br.rename("main",true).expect("Something went wrong while remaning the branch");
+    mdm::date_printer();
+    mdm::making_file();
 }
 
