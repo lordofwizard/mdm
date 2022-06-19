df8dfce764f628bd1140225824af0424323c4a80
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-06-19 19:06:26 +0530

    Functionality with proper check if the file exists or not and if it can be created

--- src/func.rs
+++ src/func.rs
@@ -4,6 +4,8 @@ pub mod mdm{
     use colored::*;
     use git2::BranchType;
     use git2::Repository;
+    use std::fs::File;
+    use std::path::Path;
 
     pub fn run(){
         let repo = match Repository::open("./data/") {
@@ -14,9 +16,30 @@ pub mod mdm{
         br.rename("main",true).expect("Something went wrong while remaning the branch");
     }
 
+    enum CanMakeFile{
+        Yes,
+        No
+    }
     
-    fn directory_maker(){
+    fn can_make_file()-> CanMakeFile{
+        let today = date_printer();
+        let path = "./data/".to_string() + &today + ".txt";
+        let path = Path::new(&path);
+        if std::path::Path::exists(path) == true{
+            println!("{}", "Hurray file already exists".blue());
+            CanMakeFile::No
+        }
+        else{
+            let path = "./data/".to_string() + &today + ".txt";
+
+            let mut result : CanMakeFile = match File::create(path.as_str()){
+                Ok(file) => CanMakeFile::Yes,
+                Err(e) => CanMakeFile::No,
+            };
+            result
+        }     
         
+
     }
     fn database_maker(){
 
@@ -30,10 +53,15 @@ pub mod mdm{
         local
     }
     pub fn making_file(){
-        use std::fs::File;
         let today = date_printer();
-        let today_str = today.as_str();
-        let path = "./data/".to_string() + &today + ".txt" ;
-        let mut file = File::create(path.as_str());
-    }
+        let path = "./data/".to_string() + &today + ".txt";
+        let variant = matches!(can_make_file(),CanMakeFile::Yes);
+        println!("Varient is {}",variant);
+        if variant == true{
+            let mut file = File::create(path.as_str()).expect("idk");
+        }
+        else{
+            println!("{}","File Already exists or something wrong with the path".red());
+        }
+        }
 }
